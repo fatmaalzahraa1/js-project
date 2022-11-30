50826d969fe08dd81a4e2059a41ec912b6420af0
fatmaalzahraa1 <fatmaalzahraa92@outlook.com>
2022-11-30 21:56:32 +0200

    task day 1

---
+++ index.js
@@ -0,0 +1,62 @@
+
+///////////task 1//////////////////
+// var temperatur = prompt("enter your  temperatur today" );
+// if (temperatur>=30)
+// {
+//     console.log('the weather is : hot')
+// }
+// else if (temperatur<=30)
+
+// {
+//     console.log('the weather is : cold')
+// }
+// else
+// {
+//    console.log('error');
+
+// var temperatur=prompt("enter your  temperatur today" );
+// (temperatur<30?console.log("cold"):console.log("hot"));
+
+
+//////////////////////////task2////////////////
+// var name =prompt("enter your name ");
+// while (isFinite(name))
+// {
+//     name=prompt("enter your name")
+// }
+// console.log(" Name is ",name);
+
+// var birthyear =prompt("enter your birthyear ");
+// while (isNaN(birthyear))
+// {
+//     birthyear=prompt("enter your birthyear")
+// }
+// console.log("birthyear is",birthyear);
+// console.log("age is",2022-birthyear);
+
+
+/////////////////task3///////////////
+// var a =prompt(("Input First Number a "));
+// var b =prompt(("Input second Number b "));
+// var c =prompt(("Input third Number c "));
+//  var x =(b*b)-(4*a*c);
+//  var r1=-b+Math.sqrt(x)/2*a;
+//  var r2=-b-Math.sqrt(x)/2*a;
+// console.log(r1);
+// console.log(r2);
+
+
+///////////////////////task4///////
+// var n =prompt("Input First Number ");
+// var m =prompt("Input Second Number ");
+// var FirstNumber=n%10;
+// var SecondNumber=m%10;
+// var sum = FirstNumber+SecondNumber;
+// console.log(sum);
+
+
+///////////////////////task5///////
+// var d1=prompt("Enter the distance:");
+// console.log("input:",d1);
+// var d2= d1*2;
+// console.log("output:",d2);
