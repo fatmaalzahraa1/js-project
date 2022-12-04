6c6a82eeda5aa36df7573be2e4807a9808655880
fatmaalzahraa1 <119163294+fatmaalzahraa1@users.noreply.github.com>
2022-12-04 22:17:17 +0200

    Add files via upload

---
+++ taskday3.js
@@ -0,0 +1,87 @@
+
+
+//task1
+// function openwindow(){
+//  newindow=window.open();
+// }
+
+// function closewindow(){
+//  newindow.close();
+// }
+
+
+//task2
+// function alrt(){
+//     alert("Clock Started");
+//     displayTime();
+// }
+// function displayTime() {
+//     var currentTime = new Date();
+//     var hours = currentTime.getHours();
+//     var minutes = currentTime.getMinutes();
+//     var seconds = currentTime.getSeconds();
+//     var clockDiv = document.getElementById('clock');
+//     clockDiv.innerText = hours + ":" + minutes + ":" + seconds;
+//     setTimeout(displayTime,1000);
+//   }
+
+
+
+//task 3
+// function openwindow()
+//  {
+//   newindow = window.open("", "", "blank");
+// }
+
+// var text = "Hello";
+// var y;
+// var i = 0;
+
+// function x() {
+
+//   document.write(text[i])
+
+//   i++
+
+//   if (i >= text.length) {
+//     clearInterval(y)
+//   }
+
+// }
+
+// y = setInterval(x, 1000);
+
+
+//task4
+// function scorling()
+// {
+//     if(scrollY>1000)
+//     {
+//         scrollTo(0,777)
+//         myFunction();
+//     }
+// }
+
+// function myFunction() {
+//     alert("hi");
+//   }
+// task5
+//   function openwindow()
+//   {
+//   newindow=window.open("","","blank");
+//   newindow.window.document.write("hii")
+//   setTimeout(closewindow,2000)
+
+// }
+
+// function closewindow(){
+//   newindow.close();
+// }
+
+
+//task6
+// function myFunction() {
+//    let myWindow = window.open("", "", "width=300,height=200");
+//    myWindow.document.write("jejkhekjhekjhekjhekjhrekjherkjhrekjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjhekjjdkabdlkdjd;kjhdq;jdhdjhwedkjjewhkjhwekjewkjhwerkjhwkjrwhrewhrwehrwehkrewhjjewrhrewhjrewhjerwkhrewhk")
+//    myWindow. scrollBy(100,100);
+//   }
